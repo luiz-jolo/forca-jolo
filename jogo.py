f7fe6a7869dada5bc572380551d5a9573bc1b553
luiz-jolo <devlujoworks@gmail.com>
2023-10-22 21:40:48 -0400

    Primeiro commit

---
+++ jogo.py
@@ -0,0 +1,16 @@
+print('#####################')
+print('Bem vindo ao clássico jogo da F O R C A')
+print('#####################')
+
+def jogar():
+    print('### F O R C A - tema: animais ###\n')
+
+    animais = ['rinoceronte', 'macaco', 'arara', 'capivara', 'jacare', 'zebra']
+    quantidadeAnimais = len(animais)
+    
+    print(quantidadeAnimais)
+    
+    
+    chute =  input('Digite uma letra\n')
+
+jogar()
